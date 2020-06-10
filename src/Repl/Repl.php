51028958c8da450c7de3a13854c7fe3bd398bead
KennedyTedesco <kennedyt.tw@gmail.com>
2020-06-10 10:04:14 -0300

    update repl

--- src/Repl/Repl.php
+++ src/Repl/Repl.php
@@ -5,31 +5,30 @@ declare(strict_types=1);
 namespace Monkey\Repl;
 
 use Monkey\Lexer\Lexer;
-use Monkey\Token\TokenType;
+use Monkey\Parser\Parser;
+use Monkey\Parser\ProgramParser;
 
 final class Repl
 {
     public function start(): void
     {
-        $welcome = <<<TEXT
-       .="=.
-     _/.-.-.\_     _
-    ( ( o o ) )    ))
-     |/  "  \|    //
-      \'---'/    //
-      /`"""`\\  ((
-     / /_,_\ \\  \\
-     \_\\_'__/ \  ))
-     /`  /`~\  |//
-    /   /    \  /
-,--`,--'\/\    /
- '-- "--'  '--'
-
-@@@@@@@@@ Monkey Programming Language @@@@@@@@@
------------------------------------------------
-TEXT;
-
-        \safe\fwrite(\STDOUT, "{$welcome}\n");
+        \safe\fwrite(\STDOUT, <<<TEXT
+               .="=.
+             _/.-.-.\_     _
+            ( ( o o ) )    ))
+             |/  "  \|    //
+              \'---'/    //
+              /`"""`\\  ((
+             / /_,_\ \\  \\
+             \_\\_'__/ \  ))
+             /`  /`~\  |//
+            /   /    \  /
+        ,--`,--'\/\    /
+         '-- "--'  '--'
+        
+        @ Monkey Programming Language @
+        --------------------------------\n\n
+        TEXT);
 
         while (true) {
             /** @var string $input */
@@ -40,12 +39,23 @@ TEXT;
 
             \safe\fwrite(\STDOUT, \PHP_EOL);
 
-            $lexer = new Lexer($input);
-            while (!$lexer->isEnd()) {
-                $token = $lexer->nextToken();
-                $name = TokenType::name($token->type());
-                \safe\fwrite(\STDOUT, "{$name}: {$token->literal()}\n");
+            $parser = new Parser(new Lexer($input));
+            $program = (new ProgramParser())($parser);
+
+            if (\count($parser->errors()) > 0) {
+                $this->printErrors($parser->errors());
+                break;
             }
+
+            \safe\fwrite(\STDOUT, $program->toString());
+        }
+    }
+
+    private function printErrors(array $errors): void
+    {
+        foreach ($errors as $index => $error) {
+            ++$index;
+            \safe\fwrite(\STDOUT, "{$index}) {$error}");
         }
     }
 }
