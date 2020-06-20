167ccc31bc5c6f457d9ba13f6675ef6fe5fd893c
KennedyTedesco <kennedyt.tw@gmail.com>
2020-06-20 12:46:10 -0300

    Update BuiltinFunction.php

--- src/Evaluator/BuiltinFunction.php
+++ src/Evaluator/BuiltinFunction.php
@@ -10,7 +10,7 @@ use Monkey\Object\InternalObject;
 
 final class BuiltinFunction
 {
-    /** @var array<InternalObject> */
+    /** @var array<BuiltinFunctionObject> */
     private static array $store;
 
     public static function set(string $name, Closure $closure): void
