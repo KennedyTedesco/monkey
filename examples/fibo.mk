d9263a882575eea5c925674eb2cd2f5ca304fd7b
KennedyTedesco <kennedyt.tw@gmail.com>
2020-06-24 19:42:36 -0300

    Update fibo.mk

--- examples/fibo.mk
+++ examples/fibo.mk
@@ -1,10 +1,6 @@
 let fibonacci = fn(x) {
-    if (x == 0) {
-        return 0;
-    }
-
-    if (x == 1) {
-        return 1;
+    if (x == 0 || x == 1) {
+        return x;
     }
 
     return fibonacci(x - 1) + fibonacci(x - 2);
