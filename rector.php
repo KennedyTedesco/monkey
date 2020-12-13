614b8d827e9361960574509504e7783761743418
Kennedy Tedesco <kennedyt.tw@gmail.com>
2020-12-13 11:04:28 -0300

    rector

---
+++ rector.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Core\Configuration\Option;
+use Rector\Set\ValueObject\SetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::SETS, [
+        SetList::PHP_74,
+        SetList::PHP_80,
+    ]);
+};
