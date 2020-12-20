61fbe7a694f17fb53f120dce9fd46f7aa2617ab7
Kennedy Tedesco <kennedyt.tw@gmail.com>
2020-12-20 12:28:34 -0300

    Update Environment.php

--- src/Evaluator/Environment.php
+++ src/Evaluator/Environment.php
@@ -9,24 +9,25 @@ use Monkey\Object\MonkeyObject;
 final class Environment
 {
     /** @var array<MonkeyObject> */
-    private array $store = [];
+    private array $map = [];
 
-    public function __construct(private ?self $outer = null)
-    {
+    public function __construct(
+        private ?self $outer = null
+    ) {
     }
 
     public function set(string $name, MonkeyObject $object): void
     {
-        $this->store[$name] = $object;
+        $this->map[$name] = $object;
     }
 
     public function get(string $name): ?MonkeyObject
     {
-        return $this->store[$name] ?? (null !== $this->outer ? $this->outer->get($name) : null);
+        return $this->map[$name] ?? (null !== $this->outer ? $this->outer->get($name) : null);
     }
 
     public function contains(string $name): bool
     {
-        return \array_key_exists($name, $this->store) || ($this->outer && $this->outer->contains($name));
+        return \array_key_exists($name, $this->map) || ($this->outer && $this->outer->contains($name));
     }
 }
