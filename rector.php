037b4d6869af116cf0689c543a2198c69318383a
Kennedy Tedesco <kennedyt.tw@gmail.com>
2021-11-30 16:45:07 -0300

    rector

--- rector.php
+++ rector.php
@@ -2,11 +2,28 @@
 
 declare(strict_types=1);
 
+use Rector\Core\Configuration\Option;
 use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
+    $parameters->set(Option::IMPORT_DOC_BLOCKS, false);
+    $parameters->set(Option::IMPORT_SHORT_CLASSES, true);
+
+    $parameters->set(Option::PATHS, [
+        __DIR__.'/src',
+    ]);
+
+    $containerConfigurator->import(SetList::DEAD_CODE);
     $containerConfigurator->import(SetList::CODE_QUALITY);
-    $containerConfigurator->import(SetList::CODE_QUALITY_STRICT);
+    $containerConfigurator->import(SetList::CODING_STYLE);
+    $containerConfigurator->import(SetList::EARLY_RETURN);
+    $containerConfigurator->import(SetList::TYPE_DECLARATION_STRICT);
+    $containerConfigurator->import(SetList::CARBON_2);
+    $containerConfigurator->import(SetList::PHP_74);
     $containerConfigurator->import(SetList::PHP_80);
+    $containerConfigurator->import(SetList::PHP_81);
 };
