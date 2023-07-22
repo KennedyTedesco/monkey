548bc7d59aa8e5f334ae3ca84e00ecba7c0981a0
Kennedy Tedesco <kennedyt.tw@gmail.com>
2023-07-22 12:31:51 -0300

    Update rector.php

--- rector.php
+++ rector.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
@@ -16,10 +15,6 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->autoloadPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
-    ]);
-
-    $rectorConfig->skip([
-        InlineConstructorDefaultToPropertyRector::class,
     ]);
 
     $rectorConfig->import(SetList::CODE_QUALITY);
