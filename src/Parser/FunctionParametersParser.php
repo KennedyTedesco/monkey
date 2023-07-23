b4a9d0359b0172e1a7ba6125490d22e76990bae8
Kennedy Tedesco <kennedyt.tw@gmail.com>
2023-07-23 14:51:23 -0300

    Update FunctionParametersParser.php

--- src/Parser/FunctionParametersParser.php
+++ src/Parser/FunctionParametersParser.php
@@ -26,12 +26,12 @@ final class FunctionParametersParser
 
         $parser->nextToken();
 
-        $identifiers[] = new IdentifierExpression($parser->curToken, $parser->curToken()->literal());
+        $identifiers[] = new IdentifierExpression($parser->curToken(), $parser->curToken()->literal());
 
         while ($parser->peekToken()->is(TokenType::COMMA)) {
             $parser->nextToken(2);
 
-            $identifiers[] = new IdentifierExpression($parser->curToken, $parser->curToken()->literal());
+            $identifiers[] = new IdentifierExpression($parser->curToken(), $parser->curToken()->literal());
         }
 
         if (!$parser->expectPeek(TokenType::RPAREN)) {
